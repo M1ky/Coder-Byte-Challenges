fd63bb0ec5b0fc83a07de7fa6e8d58930f34ab20
M1ky <32638698+M1ky@users.noreply.github.com>
2019-05-07 08:01:48 +0200

    Update second_great_low.py

--- second_great_low.py
+++ second_great_low.py
@@ -1,7 +1,8 @@
-# Have the function SecondGreatLow(arr) take the array of numbers stored in arr and return the second lowest and second greatest numbers, 
-# respectively, separated by a space. For example: if arr contains [7, 7, 12, 98, 106] the output should be 12 98. 
-# The array will not be empty and will contain at least 2 numbers. It can get tricky if there's just two numbers! 
-
+'''
+Have the function SecondGreatLow(arr) take the array of numbers stored in arr and return the second lowest and second greatest numbers, 
+respectively, separated by a space. For example: if arr contains [7, 7, 12, 98, 106] the output should be 12 98. 
+The array will not be empty and will contain at least 2 numbers. It can get tricky if there's just two numbers! 
+'''
 
 def SecondGreatLow(arr):
 	arr.sort()
@@ -18,7 +19,6 @@ def SecondGreatLow(arr):
 		no_duplicate_arr.sort()
 
 		return ("{} {}".format(no_duplicate_arr[1], no_duplicate_arr[-2]))
-
 
 
 
